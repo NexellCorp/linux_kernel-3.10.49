8981ecfa293e8503e008866ed76cbaba9be61152
jhkim <jhkim@nexell.co.kr>
2015-09-11 20:14:31 +0900

    MOD: ARM72_COMPAT config and cpu info for android

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -507,6 +507,14 @@ static int c_show(struct seq_file *m, void *v)
 		if (elf_hwcap & (1 << i))
 			seq_printf(m, "%s ", hwcap_str[i]);
 
+#ifdef CONFIG_ARMV7_COMPAT_CPUINFO
+    if (is_compat_task()) {
+		/* Print out the non-optional ARMv8 HW capabilities */
+		seq_printf(m, "wp half thumb fastmult vfp edsp neon vfpv3 tlsi ");
+		seq_printf(m, "vfpv4 idiva idivt ");
+	}
+#endif
+
 	seq_printf(m, "\nCPU implementer\t: 0x%02x\n", read_cpuid_id() >> 24);
 	seq_printf(m, "CPU architecture: AArch64\n");
 	seq_printf(m, "CPU variant\t: 0x%x\n", (read_cpuid_id() >> 20) & 15);
